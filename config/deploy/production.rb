15b553daf6166c63f70d670d098e301669b67d8a
root <rahul>
2018-05-13 01:44:36 +0530

    deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,14 +14,14 @@ set :stage, :production
 
 # Define roles, user and IP address of deployment server
 # role :name, %{[user]@[IP adde.]}
-role :app, %w{ec2-user@13.127.182.169}
+role :app, %w{ec2-user@13.127.182.***}
 	# 172.31.6.54}
 
 # Define server(s)
 # Example:
 # server '[your droplet's IP addr]', user: '[the deployer user]', roles: %w{[role names as defined above]}
 # server '162.243.74.190', user: 'deployer', roles: %w{app}
-server '13.127.182.169', user: 'ec2-user', roles: %w{app}
+server '13.127.182.***', user: 'ec2-user', roles: %w{app}
 set :use_sudo, true
 
 # SSH Options 
